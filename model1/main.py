eed2a8d4eeafdb64a08c89a9ebca9f17fff3ded1
garfbild <30866252+garfbild@users.noreply.github.com>
2020-07-15 19:12:06 +0100

    Update main.py

--- model1/main.py
+++ model1/main.py
@@ -2,30 +2,32 @@ from PIL import Image
 import time
 import os
 import numpy as np
+import tensorflow as tf
 from matplotlib import pyplot as plt
 from keras.models import Sequential, model_from_json, load_model
 from keras.layers import Dense, Activation, Conv2D, Flatten, MaxPooling2D, Dropout
 from keras.utils import to_categorical
+from keras.applications.resnet50 import ResNet50
 
 def loadTrainData():
     x_train = []
     y_train = []
-    names = os.listdir(r"C:\Users\louis\Documents\STL-10\train_images")[0:100]
+    names = os.listdir(r"C:\Users\louis\Documents\GlassesData\train_images")[0:100]
     for name in names:
-        img = Image.open("C:\\Users\\louis\\Documents\\STL-10\\train_images\\"+name)
+        img = Image.open("C:\\Users\\louis\\Documents\\GlassesData\\train_images\\"+name)
         data = np.asarray(img, dtype = "int32")
         x_train.append(data)
         y_train.append(0)
 
-    for name in os.listdir(r"C:\Users\louis\Documents\STL-10\Glasses\negative"):
-        img = Image.open("C:\\Users\\louis\\Documents\\STL-10\\Glasses\\negative\\"+name)
+    for name in os.listdir(r"C:\Users\louis\Documents\GlassesData\Glasses\negative"):
+        img = Image.open("C:\\Users\\louis\\Documents\\GlassesData\\Glasses\\negative\\"+name)
         img = img.resize((96,96))
         data = np.asarray(img, dtype = "int32")
         x_train.append(data)
         y_train.append(0)
 
-    for name in os.listdir(r"C:\Users\louis\Documents\STL-10\Glasses\positive"):
-        img = Image.open("C:\\Users\\louis\\Documents\\STL-10\\Glasses\\positive\\"+name)
+    for name in os.listdir(r"C:\Users\louis\Documents\GlassesData\Glasses\positive"):
+        img = Image.open("C:\\Users\\louis\\Documents\\GlassesData\\Glasses\\positive\\"+name)
         img = img.resize((96,96))
         data = np.asarray(img, dtype = "int32")
         x_train.append(data)
@@ -87,15 +89,15 @@ def makePrediction(model,data):
 def loadTestData():
     x_test = []
     y_test = []
-    names = os.listdir(r"C:\Users\louis\Documents\STL-10\test_images")[0:10]
+    names = os.listdir(r"C:\Users\louis\Documents\GlassesData\test_images")[0:10]
     for name in names:
-        img = Image.open("C:\\Users\\louis\\Documents\\STL-10\\test_images\\"+name)
+        img = Image.open("C:\\Users\\louis\\Documents\\GlassesData\\test_images\\"+name)
         data = np.asarray(img, dtype = "int32")
         x_test.append(data)
         y_test.append(0)
 
-    for name in os.listdir(r"C:\Users\louis\Documents\STL-10\Glasses_cv"):
-        img = Image.open("C:\\Users\\louis\\Documents\\STL-10\\Glasses_cv\\"+name)
+    for name in os.listdir(r"C:\Users\louis\Documents\GlassesData\Glasses_cv"):
+        img = Image.open("C:\\Users\\louis\\Documents\\GlassesData\\Glasses_cv\\"+name)
         img = img.resize((96,96))
         data = np.asarray(img, dtype = "int32")
         x_test.append(data)
@@ -106,46 +108,53 @@ def loadTestData():
 
     return x, y
 
-model = createModel()
-model = trainModel(model,"cnn5")
+def regionProposal():
+    img = Image.open("C:\\Users\\louis\\Documents\\GlassesData\\long shot\\20200708_000552.jpg")
+    width, height = img.size
+    length = int(img.size[0]/5)
+    step = int(length/4)
+    l = length/2 #because square
+
+
+    max = 0
+    maxi, maxj = 0, 0
+
+    i = 0
+    while i*step + length <= width:
+        j = 0
+        while j*step + length <= height:
+            temp = img.crop((i*step, j*step,i*step + length, j*step + length))
+            temp = temp.resize((96,96))
+            data = np.asarray(temp, dtype = "int32")
+            classes = makePrediction(model,data)
+            if classes[0][1] > max:
+                plt.imshow(data, cmap='hsv')
+                plt.pause(0.5)
+                print(i,j,classes[0])
+                max = classes[0][1]
+                maxi, maxj = i, j
+            j += 1
+        i += 1
+
+    temp = img.crop((maxi*step, maxj*step,maxi*step + length, maxj*step + length))
+    temp = temp.resize((96,96))
+    data = np.asarray(temp, dtype = "int32")
+    plt.imshow(data, cmap='hsv')
+    plt.show()
+
+#model = createModel()
+#model = trainModel(model,"cnn5")
 
 #model = load_model("cnn5.h5")
 
-x, y = loadTestData()
+#x, y = loadTestData()
 
-print(makePrediction(model,x))
-print(y)
+#print(makePrediction(model,x))
+#print(y)
 
-
-img = Image.open("C:\\Users\\louis\\Documents\\STL-10\\long shot\\20200708_000552.jpg")
-width, height = img.size
-length = int(img.size[0]/5)
-step = int(length/4)
-l = length/2 #because square
-
-
-max = 0
-maxi, maxj = 0, 0
-
-i = 0
-while i*step + length <= width:
-    j = 0
-    while j*step + length <= height:
-        temp = img.crop((i*step, j*step,i*step + length, j*step + length))
-        temp = temp.resize((96,96))
-        data = np.asarray(temp, dtype = "int32")
-        classes = makePrediction(model,data)
-        if classes[0][1] > max:
-            plt.imshow(data, cmap='hsv')
-            plt.pause(0.5)
-            print(i,j,classes[0])
-            max = classes[0][1]
-            maxi, maxj = i, j
-        j += 1
-    i += 1
-
-temp = img.crop((maxi*step, maxj*step,maxi*step + length, maxj*step + length))
-temp = temp.resize((96,96))
-data = np.asarray(temp, dtype = "int32")
-plt.imshow(data, cmap='hsv')
-plt.show()
+model = Sequential()
+model.add(ResNet50(include_top = False, weights = 'imagenet'))
+model.add(Dense(2))
+for layer in model.layers[0].layers:
+    layer.trainable = False
+model.summary()
